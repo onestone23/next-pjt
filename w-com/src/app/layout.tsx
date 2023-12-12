41e9afe2644f54aeb5b76949b861b076b19479bd
onestone23 <lws2269@kakao.com>
2023-12-12 15:53:14 +0900

    ch1 스타일

--- w-com/src/app/layout.tsx
+++ w-com/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import styles from './page.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +13,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>루트레이아웃{children}</body>
+      <body className={inter.className}>
+        <div className={styles.container}>{children}</div>
+      </body>
     </html>
   );
 }

5538f53159fefbc117995f0f2462a1049ce552eb
onestone23 <lws2269@kakao.com>
2023-12-20 11:08:42 +0900

    클라이언트

--- w-com/src/app/(beforeLogin)/layout.tsx
+++ w-com/src/app/(beforeLogin)/layout.tsx
@@ -1,8 +1,12 @@
 import styles from '@/app/page.module.css';
 
-export default function Layout({ children, modal }: { children: React.ReactNode; modal: React.ReactNode }) {
+type Props = { children: React.ReactNode; modal: React.ReactNode };
+
+export default function Layout({ children, modal }: Props) {
   return (
     <div className={styles.container}>
+      {/* 패러럴 모달의 경우 페이지가 같은 라인에 있어야함 = app->(beforeLogin) */}
+      {/* 골뱅이 뒤의 폴더명을 따라감 @mdoal -> modal , @hello -> hello */}
       {children}
       {modal}
     </div>

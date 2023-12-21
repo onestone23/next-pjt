4c5c007575b48028059957ff9ae06b91762e0572
onestone23 <lws2269@kakao.com>
2023-12-21 10:53:31 +0900

    chore

--- w-com/src/app/layout.tsx
+++ w-com/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import styles from './page.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,9 +14,7 @@ type Props = { children: React.ReactNode };
 export default function RootLayout({ children }: Props) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
-        <div className={styles.container}>{children}</div>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
